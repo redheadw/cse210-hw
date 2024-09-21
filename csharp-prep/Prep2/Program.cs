6e3ea0ef58fcdbfa0bc08171842ac2980cfccb69
redheadw <wrightinthered@att.net>
2024-09-20 20:50:21 -0500

    letter grade

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,66 @@
 using System;
+using System.Runtime.InteropServices.Marshalling;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        //What is your letter grade?
+        Console.Write("Enter number grade: ");
+        string valuefromUser = Console.ReadLine();
+        int x = int.Parse(valuefromUser);
+        
+        string letter = "";
+        if (x >= 90)
+        {
+            letter = "A"; 
+        }
+        
+        else if (x >= 80)
+        {
+            letter = "B";
+        }
+        else if (x >= 70)
+        {
+            letter = "C";
+        }
+        else if (x >= 60)
+        {
+            letter = "D";
+        }
+        else 
+        {
+            letter = "F";
+        }
+
+        
+
+        if (x >= 70)
+        {
+            Console.WriteLine("Congradulation you pass!");
+        }
+        else 
+        {
+            Console.WriteLine("How can we help you improve your grade?");
+        }
+
+        // Stretch Challenge
+        string sign= "";
+        int lastDigit = x % 10;
+
+        if (lastDigit > 7)
+        {
+            sign = "+";
+        }
+        else if (lastDigit < 3)
+        {
+            sign = "-";
+        }
+        else 
+        {
+            sign = " ";
+        }
+        Console.WriteLine($"Your grade is: {letter} {sign}");
     }
+
 }
