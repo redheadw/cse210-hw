ac62a71af2f58a3d36f39578b66b3d5b522e2ed9
redheadw <wrightinthered@att.net>
2024-09-20 21:31:03 -0500

    random numbers

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // magic number game
+        Random randomGenerator = new Random();
+        int magicnumber = randomGenerator.Next(1, 100);
+
+        int guess = -1;
+        int guessCount = 0;
+
+        Console.WriteLine("Can you guess the number?");
+        // loop
+        while (guess != magicnumber)
+        {
+            Console.WriteLine("What is your guess? ");  
+            guess = int.Parse(Console.ReadLine());
+            guessCount++;
+
+            if (magicnumber > guess)
+            {
+                Console.WriteLine("Higher");
+            } 
+            else if (magicnumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
+        Console.WriteLine($"You guessed {guessCount} times");
+        Console.Write("Do you want to play again? (yes/no):  ");
+        
+    Console.WriteLine("Thanks for playing!");    
+        
     }
 }
