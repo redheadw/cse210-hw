343438817bb965d838425942e48328cda227cca1
redheadw <wrightinthered@att.net>
2024-10-15 08:55:27 -0500

    main

--- prepare/Learning06/Program.cs
+++ prepare/Learning06/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning06 World!");
+       // Test MathAssignment//
+       MathAssignment mathAssignment = new MathAssignment("Gwyn Wright", "Fractions", "5.02", "6-17");
+       Console.WriteLine(mathAssignment.GetSummary());
+       Console.WriteLine(mathAssignment.GetHomeworkList());
+
+       // Test WritingAssingment
+       WritingAssignment writingAssignment = new WritingAssignment("Gwyn Wright", "Programing with Classes", "Inheritance");
+       Console.WriteLine(writingAssignment.GetSummary());
+       Console.WriteLine(writingAssignment.GetWritingInformation()); 
     }
 }
---
+++ prepare/Learning06/WritingAssignment.cs
@@ -0,0 +1,14 @@
+using System;
+// WritingAssignment.cs
+public class WritingAssignment : Assignment
+{
+    private string _title;
+    public WritingAssignment(string studentName, string topic, string title) : base(studentName, topic)
+    {
+        _title = title;
+    }
+    public string GetWritingInformation()
+    {
+        return $"{_title} by {GetStudentName()}";
+    }
+}
