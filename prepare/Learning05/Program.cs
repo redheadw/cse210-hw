ad99b8c396a5f5de662ef9b3fee3bbb905644c86
redheadw <wrightinthered@att.net>
2024-10-22 19:30:13 -0500

    Shapes

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        //List of shapes
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(new Square("blue", 5));
+        shapes.Add(new Rectangle("green", 2, 6));
+        shapes.Add(new Circle("purple", 3.25));
+
+        //display color and area of each shape
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Shape Color: {shape.GetColor()}, Area {shape.GetArea()}");
+        }
+
     }
 }
